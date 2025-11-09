b70aec98c3d17143dbdaf9acd32e622c535832d8
prathamhere5-glitch <prathamhere5@gmail.com>
2025-11-09 22:37:01 +0530

    Delete scheduler.js

--- scheduler.js
+++
@@ -1,7 +0,0 @@
-import schedule from "node-schedule";
-
-export const scheduleMessage = (client, chatId, text, time) => {
-  schedule.scheduleJob(time, () => {
-    client.sendMessage(chatId, text);
-  });
-};

cc1bb7f21ab76cedc833435726bf23a02b88a487
prathamhere5-glitch <prathamhere5@gmail.com>
2025-11-09 22:36:51 +0530

    Delete ui.js

--- ui.js
+++
@@ -1,9 +0,0 @@
-import { Markup } from "telegraf";
-
-export const mainMenu = Markup.inlineKeyboard([
-  [Markup.button.callback("➕ Add Account", "ADD_ACCOUNT")],
-  [Markup.button.callback("📄 List Linked Accounts", "LIST_ACCOUNTS")],
-  [Markup.button.callback("🔢 Pairing Code Login", "PAIRING")],
-  [Markup.button.callback("🔳 QR Login", "QR_LOGIN")],
-  [Markup.button.callback("🛑 Stop", "STOP")]
-]).resize();
